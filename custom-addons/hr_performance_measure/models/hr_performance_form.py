80c8cd0511bed85f52cbb9e58761fe4c78af0d68
Kuma Telila <kumatelila26@gmail.com>
2024-11-21 15:14:48 +0300

    Rating Factors done

--- custom-addons/hr_performance_measure/models/hr_performance_form.py
+++ custom-addons/hr_performance_measure/models/hr_performance_form.py
@@ -6,12 +6,7 @@ class HrPerformanceForm(models.Model):
     _description = "HR Performance Form"
 
     name = fields.Char(string="Form Name", required=True, help="Name of the performance form (e.g., Job Performance, Teamwork).")
-    rating_factors = fields.One2many(
-        "hr.performance.form.factor",
-        "form_id",
-        string="Rating Factors",
-        help="List of factors that will be evaluated in this form."
-    )
+    description = fields.Html(string='Job Description', sanitize_attributes=False)
 
     @api.constrains('rating_factors')
     def _check_rating_factors(self):
@@ -19,8 +14,3 @@ class HrPerformanceForm(models.Model):
         for record in self:
             if not record.rating_factors:
                 raise ValidationError("You must add at least one rating factor to the form.")
-
-class HrPerformanceFormFactor(models.Model):
-    _name = "hr.performance.form.factor"
-    _description = "HR Performance Form Rating Factor"
-    name = fields.Char(string="Rating Factor", required=True, help="The factor being evaluated (e.g., Productivity, Attendance).")
