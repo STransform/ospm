3bcf19a35747283c58b4524fa059c276351cd098
chalaa <chalaolanigeleta@gmail.com>
2024-11-26 16:00:27 +0300

    Add Transfer History

--- custom-addons/transfer/models/hr_employee.py
+++ custom-addons/transfer/models/hr_employee.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    transfer_ids = fields.Many2many('transfer.request', string='Transfer',
-                                     help='The Transfer that an'
-                                          ' employee has received')
+    # transfer_ids = fields.Many2many('transfer.request', string='Transfer',
+    #                                  help='The Transfer that an'
+    #                                       ' employee has received')
+    transfer_history_ids = fields.One2many('transfer.history', 'employee_id', string="Transfer History")
