ec15b312016f9ed5141a2e0cece57b305290ab7c
Kuma Telila <kumatelila26@gmail.com>
2024-12-02 14:26:34 +0300

    fields for salary increment and tree view added

--- custom-addons/hr_salary_increment/models/hr_salary_increment.py
+++ custom-addons/hr_salary_increment/models/hr_salary_increment.py
@@ -5,5 +5,32 @@ from odoo.exceptions import ValidationError, AccessError
 class HrSalaryIncrement(models.Model):
     _name = "hr.salary.increment"
     _description = "Hr Salary Increment"
-    
-    employee_id = fields.Many2one('hr.employee', string="Employee", required=True, tracking=True)
+    _rec_name = "employee_id"
+    _order = "create_date desc"
+
+    employee_id = fields.Many2one(
+        "hr.employee", string="Employee", required=True, tracking=True
+    )
+    contract_id = fields.Many2one(
+        "hr.contract",
+        string="Contract",
+        related="employee_id.contract_id",
+        tracking=True,
+    )
+    wage_id = fields.Float(
+        string="Wage per Month",
+        related="employee_id.contract_id.wage",
+        readonly=True,
+    )
+    currency_id = fields.Many2one("res.currency", related="contract_id.currency_id")
+    grade_id = fields.Many2one(
+        "hr.pay.grade",
+        string="Pay Grade",
+        related="employee_id.contract_id.pay_grade_id",
+        readonly=True,
+    )
+    increment_id = fields.Many2one(
+        "hr.pay.grade.increment",
+        string="Increment",
+        related="employee_id.contract_id.increment_level_id",
+    )
