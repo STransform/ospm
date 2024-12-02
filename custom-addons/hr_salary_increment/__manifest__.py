0b8f1c075a5fb5909f6870623b5313e652d2cfcf
Kuma Telila <kumatelila26@gmail.com>
2024-12-02 11:07:46 +0300

    salary increment module created

---
+++ custom-addons/hr_salary_increment/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hr_salary_increment',
+    'version': '',
+    'summary': """ Hr_salary_increment Summary """,
+    'author': 'Kuma Telila @otech',
+    'website': 'https://otech.et',
+    'category': 'Human Resource',
+    'depends': ['base', 'hr','hr_pay_grade', 'mail'],
+    'data': [
+        
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
