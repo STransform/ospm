b735f43829e9bfafe58d693ace9adb25ae9a48d9
Kuma Telila <kumatelila26@gmail.com>
2024-12-10 15:01:07 +0300

    Overtime rate type done

---
+++ custom-addons/hr_overtime_payment/models/hr_overtime_rate.py
@@ -0,0 +1,23 @@
+from odoo import api, models, fields
+
+
+class HrOvertimeType(models.Model):
+    _name = "hr.overtime.rate"
+    _description = "Overtime Rate Types"
+    _order = "create_date desc"
+
+    name = fields.Selection(
+        [
+            ("normal_day", "Regular Day (6 a.m. to 10 p.m.)"),
+            ("night_shift", "Night Shift (10 p.m. to 6 a.m.)"),
+            ("weekly_rest", "Weekly Rest Day"),
+            ("public_holiday", "Public Holiday"),
+        ],
+        string="Overtime Type",
+        required=True,
+    )
+    hourly_rate = fields.Float(string="Hourly Rate (%)", required=True, tracking=True)
+
+    _sql_constraints = [
+        ("unique_rate_type", "UNIQUE(name)", "The overtime rate type must be unique.")
+    ]
