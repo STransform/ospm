5ae456a5c37bdbd9ca3386992ddc46d0f34b57f1
Kuma Telila <kumatelila26@gmail.com>
2024-12-13 10:46:07 +0300

    user group and web notification done

--- custom-addons/hr_overtime_payment/models/hr_overtime_payment.py
+++ custom-addons/hr_overtime_payment/models/hr_overtime_payment.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from datetime import datetime
+from babel.dates import format_date
 
 
 class HrOvertimePayment(models.Model):
@@ -52,6 +53,24 @@ class HrOvertimePayment(models.Model):
         store=True,
         tracking=True,
     )
+    
+
+    # Format the date to a human-readable format
+    def _format_date(self, date):
+        if date:
+            return format_date(date, format="long", locale=self.env.user.lang or "en_US")
+        return ""
+
+     # add notification function 
+    @api.model
+    def send_notification(self, message, user, title):
+        self.env['custom.notification'].create({
+            'title': title,
+            'message': message,
+            'user_id': user.id,
+        })
+
+
 
     @api.depends("employee_id")
     def _compute_wage(self):
@@ -61,6 +80,12 @@ class HrOvertimePayment(models.Model):
             else:
                 record.wage = 0.0
 
+    
+    # clear list onchange eployee
+    @api.onchange("employee_id")
+    def _clear_payment_items(self):
+        for record in self:
+            record.overtime_payment_item_ids = None
     # total amount calculation
     @api.depends("overtime_payment_item_ids.amount")
     def _compute_total_amount(self):
@@ -68,8 +93,18 @@ class HrOvertimePayment(models.Model):
             record.total_amount = sum(
                 item.amount for item in record.overtime_payment_item_ids
             )
-
+            
     def action_submit(self):
+        # notification
+        ## search users with specific group
+        hr_office = self.env.ref("user_group.group_hr_office").users
+        title = "New Request for Overtime Payment"
+        message = f"New Request to be approved."
+        for user in hr_office:
+            self.send_notification(message, user, title) 
+            user.notify_success(title=title, message=message)
+        self.env.user.notify_success("Request Submitted")
+        
         self.state = "submitted"
 
     def action_approve(self):
@@ -84,6 +119,14 @@ class HrOvertimePayment(models.Model):
             }
         )
         self.state = "approved"
+        ## search users with specific group
+        department_manager = self.env.ref("user_group.group_department_manager").users
+        title = "Overtime Payment Approved"
+        message = f"approved."
+        for user in department_manager:
+            self.send_notification(message, user, title) 
+            user.notify_success(title=title, message=message)
+        self.env.user.notify_success("Request Successfully Approved")
 
     def action_reject(self):
         """Reject the batch."""
@@ -95,3 +138,4 @@ class HrOvertimePayment(models.Model):
             "target": "new",
             "context": {"default_rejection_reason": self.rejection_reason},
         }
+        
