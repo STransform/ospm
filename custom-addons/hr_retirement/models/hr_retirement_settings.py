e1e0b4203a24f434c9c7e9f186610086f973851c
Chala <chalaolanigeleta@gmail.com>
2024-12-14 05:37:16 +0000

    Resolve "integrate notification and update user group chala"

--- custom-addons/hr_retirement/models/hr_retirement_settings.py
+++ custom-addons/hr_retirement/models/hr_retirement_settings.py
@@ -7,7 +7,7 @@ class HrRetirementSettings(models.Model):
     _rec_name = 'name'
 
     # Fields
-    name =  fields.Char(string="Conficuration", default = "Retirement Configuration")
+    name =  fields.Char(string="Configuration", default = "Retirement Configuration")
     max_service_years = fields.Integer(string="Maximum Service Years", required=True, default=30)
     retirement_age = fields.Integer(string="Retirement Age", required=True, default=60)
     retirement_threshold_months = fields.Integer(string="Retirement Threshold (Months)", required=True, default=12)
